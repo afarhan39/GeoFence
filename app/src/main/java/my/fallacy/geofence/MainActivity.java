c15323d13f35462f8ceb4574c576ed8c64213b93
afarhan <amir.farhan130794@gmail.com>
2019-02-27 10:36:54 +0800

    first commit

---
+++ app/src/main/java/my/fallacy/geofence/MainActivity.java
@@ -0,0 +1,14 @@
+package my.fallacy.geofence;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
